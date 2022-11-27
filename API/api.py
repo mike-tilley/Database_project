e9aab3d953f7801e40126314a95e9028f8179eb1
mike-tilley <99358410+mike-tilley@users.noreply.github.com>
2022-11-27 09:49:28 -0500

    abstracted infrastructure functions

--- API/api.py
+++ API/api.py
@@ -1,35 +1,9 @@
 import json
 from flask import Flask, request, Response
-import mysql.connector
-
-db_user = "root"
-db_password = "password"
-db_host = "localhost"
-db_name = "databaseproject"
+from infastructure_files.support import *
 
 # creating web framework functionality
 app = Flask(__name__)
-
-def connect_to_database():
-    connection = mysql.connector.connect(user=db_user, password=db_password,
-                                         host=db_host, database=db_name)
-    cursor = connection.cursor()
-    return connection, cursor
-
-def unpack_results(stored_results):
-    results = []
-    col_names = []
-    for result in stored_results:
-        # Get the column names from the first data entry
-        if len(col_names) == 0:
-            for desc in result.description:
-                col_names.append(desc[0])
-        for row in result.fetchall():
-            formatted_row = {}
-            for i in range(len(col_names)):
-                formatted_row.update({col_names[i]: row[i]})
-            results.append(formatted_row)
-    return results
 
 # routes go here
 @app.route("/")
@@ -40,17 +14,43 @@ def index():
 def ping():
     return Response(json.dumps("PONG"), status=200, mimetype="application/json")
 
-@app.route("/register")
+@app.route("/register", methods=["POST"])
 def register():
-    return Response(json.dumps("this is the future, believe it or not"), status=200, mimetype="application/json")
+    """ Add the user to the user table."""
+    email = "test@okay.com"
+    first_name = "test"
+    last_name = "TEST"
+    password = "testPassword"
+
+    try:
+        # create a connection to the database
+        connection, cursor = connect_to_database()
+        # use the cursor to send user info to db
+        cursor.callproc("i_user", email, first_name, last_name, password)
+        results = unpack_results(stored_results=cursor.stored_results())
+        print(results)
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Exception as error:
+        message = f"and error occurred: {error}"
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals():
+            connection.close()
+        return Response(json.dumps(message), status=200, mimetype="application/json")
+    return Response(json.dumps(f"user {email} inserted"), status=200, mimetype="application/json")
+
+    
 
 @app.route("/users", methods=["GET"])
 def users():
+    """ bring back the user table. """
     try:
         # create a connection to the database
         connection, cursor = connect_to_database()
         # use the cursor to call the get the user table
-        cursor.callproc("g_usertable")
+        cursor.callproc("s_usertable")
         users = unpack_results(stored_results=cursor.stored_results())
         print(users)
         connection.commit()
