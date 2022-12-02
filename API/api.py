3553ae00d9bff73db4ee46541a326ba03205be77
mike-tilley <99358410+mike-tilley@users.noreply.github.com>
2022-12-02 08:03:00 -0500

    updated css fixed app.js to hook up to api todo: fix api end points used for testing

--- API/api.py
+++ API/api.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, request, Response
 from infastructure_files.support import *
+from datetime import date
 
 # creating web framework functionality
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app = Flask(__name__)
 # routes go here
 @app.route("/")
 def index():
+    # return json.dumps("home page")
     return Response(json.dumps("home page"), status=200, mimetype="application/json")
 
 @app.route("/ping")
@@ -64,6 +66,69 @@ def users():
             connection.close()
         return Response(json.dumps(message), status=200, mimetype="application/json")
     return Response(json.dumps(users), status=200, mimetype="application/json")
+
+
+@app.route("/delete_user", methods=["GET"])
+def delete_user():
+    """ delete a user. """
+    user_id = 3
+
+    try:
+        # create a connection to the database
+        connection, cursor = connect_to_database()
+        # use the cursor to call the get the user table
+        cursor.callproc("d_user",(user_id,))
+        confirm = unpack_results(stored_results=cursor.stored_results())
+        print(confirm)
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Exception as error:
+        message = f"and error occurred: {error}"
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals():
+            connection.close()
+        return Response(json.dumps(message), status=200, mimetype="application/json")
+
+@app.route("/make_survey", methods=["GET"])
+def make_survey():
+    """ bring back the user table. """
+    # survery table stuff
+    user_id = 2
+    start = date(2022, 12, 1)
+    end = date(2022, 12, 25)
+    participants = 1
+    description = "test survey for insertion mapping"
+
+    # questionaire stuff
+
+    q1 = "how old are you"
+    a1 = "1000"
+    q2 = "where do you live"
+    a2 = "Florida"
+    q3 = "how much do you like this project"
+    a3 = "5"
+   
+    try:
+        # create a connection to the database
+        connection, cursor = connect_to_database()
+        # use the cursor to call the get the user table
+        cursor.callproc("i_survey",(user_id, start, end, participants, description, q1, a1, q2, a2, q3, a3))
+        confirm = unpack_results(stored_results=cursor.stored_results())
+        print(confirm)
+        connection.commit()
+        cursor.close()
+        connection.close()
+    except Exception as error:
+        message = f"and error occurred: {error}"
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals():
+            connection.close()
+        return Response(json.dumps(message), status=200, mimetype="application/json")
+    return Response(json.dumps(f"The user id: {user_id} had a survey created."), status=200, mimetype="application/json")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('/users', {
+      'methods': 'GET',
+      headers:{
+        "Content-Type": "applications/json"
+      }
+      }).then(resp => resp.json())
+      .then(resp => console.log(resp))
+      .catch(error => console.log(error))
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Database Project Group 6 </h1>
     </div>
   );
 }
